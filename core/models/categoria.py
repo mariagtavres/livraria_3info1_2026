0f34a29be061f31ca3860dca2ae83b925771b794
mariagtavres <mariagoncalves.ifc.araquari@gmail.com>
2026-02-20 10:52:36 -0300

    feat: criação da model categoria

---
+++ core/models/categoria.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    ...
+    def __str__(self):
+        return self.descricao.upper()
+# return f"({self.id} self.descricao)"
